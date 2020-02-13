733524ac47d810d6fc117f08b595b490cabb10ed
kroetenjunge <frahm.coding@gmail.com>
2020-02-13 08:56:54 +0100

    New GUI approach

--- src/jetengine/gui/MainFrame.java
+++ src/jetengine/gui/MainFrame.java
@@ -1,41 +1,76 @@
 package jetengine.gui;
 
 import javax.swing.JFrame;
-import javax.swing.JToolBar;
 import java.awt.BorderLayout;
+import javax.swing.JDesktopPane;
 import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Color;
+import javax.swing.JInternalFrame;
+import javax.swing.border.MatteBorder;
 import javax.swing.JSplitPane;
 
 public class MainFrame extends JFrame {
 	public MainFrame() {
+		//Eigenschaften MainFrame
+		setTitle("JetEngine");
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		setBounds(100, 100, 1280, 720);
 		
-		JToolBar toolBar = new JToolBar();
-		getContentPane().add(toolBar, BorderLayout.NORTH);
+		JDesktopPane desktopPane = new JDesktopPane();
+		desktopPane.setBackground(Color.GRAY);
+		getContentPane().add(desktopPane, BorderLayout.CENTER);
 		
+		//Inhalte JDesktopPane
+		JInternalFrame RegsFlags = new JInternalFrame("Register & Flags");
+		RegsFlags.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		RegsFlags.setVisible(true);
+		GroupLayout gl_desktopPane = new GroupLayout(desktopPane);
+		gl_desktopPane.setHorizontalGroup(
+			gl_desktopPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_desktopPane.createSequentialGroup()
+					.addComponent(RegsFlags, GroupLayout.PREFERRED_SIZE, 255, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(1009, Short.MAX_VALUE))
+		);
+		gl_desktopPane.setVerticalGroup(
+			gl_desktopPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_desktopPane.createSequentialGroup()
+					.addComponent(RegsFlags, GroupLayout.PREFERRED_SIZE, 266, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(394, Short.MAX_VALUE))
+		);
+		RegsFlags.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		//Inhalte JInternalPane: RegsFlags
 		JSplitPane splitPane = new JSplitPane();
-		splitPane.setDividerSize(10);
-		getContentPane().add(splitPane, BorderLayout.CENTER);
+		RegsFlags.getContentPane().add(splitPane, BorderLayout.CENTER);
+		desktopPane.setLayout(gl_desktopPane);
 		
-		JSplitPane splitPane_1 = new JSplitPane();
-		splitPane_1.setDividerSize(10);
-		splitPane_1.setOrientation(JSplitPane.VERTICAL_SPLIT);
-		splitPane.setRightComponent(splitPane_1);
-		
-		JSplitPane splitPane_2 = new JSplitPane();
-		splitPane_2.setDividerSize(10);
-		splitPane_1.setLeftComponent(splitPane_2);
-		splitPane_2.setDividerLocation(400);
-		splitPane_1.setDividerLocation(400);
-		splitPane.setDividerLocation(150);
-		
+		//Menu Bar + Inhalte
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		pack();
+		
+		JMenu mnFiles = new JMenu("Datei");
+		menuBar.add(mnFiles);
+		
+		JMenuItem mntmFileOpen = new JMenuItem("Datei \u00F6ffnen");
+		mnFiles.add(mntmFileOpen);
+		
+		JMenuItem mntmFileSave = new JMenuItem("Speichern");
+		mnFiles.add(mntmFileSave);
+		
+		JMenuItem mntmExit = new JMenuItem("Beenden");
+		mnFiles.add(mntmExit);
+		
+		JMenu mnHelp = new JMenu("Hilfe");
+		menuBar.add(mnHelp);
+		
 	}
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
 }
